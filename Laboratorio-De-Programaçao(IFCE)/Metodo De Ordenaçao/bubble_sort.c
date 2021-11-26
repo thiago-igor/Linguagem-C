5d0edfbe9d4e27e95efbb1b54320632dc91c8c12
thiago-igor <thiago.maia0606@gmail.com>
2021-11-25 23:50:09 -0300

    atualizaçao

---
+++ Laboratorio-De-Programaçao(IFCE)/Metodo De Ordenaçao/bubble_sort.c
@@ -0,0 +1,51 @@
+
+#include <stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#define TAM 100
+
+int main(void) {
+  
+  int k,j,x,y, vetor[TAM];
+  int t;
+  int *pv;
+
+  srand(time(NULL));
+
+  printf("escolha um intervalo entre \n[X]:");
+  scanf("%d",&x);
+  printf("[Y]:");
+  scanf("%d",&y);
+
+  pv = vetor;
+
+  for(k=0;k<TAM;k++){
+    //vetor[k] = x+rand()%(y+1-x);
+    *(pv+k) = x+rand()%(y+1-x);
+  }
+
+  printf("sequencia gerada =>");
+  for(k=0;k<TAM;k++){
+    printf("%d,",*(pv+k));
+  }
+  
+  printf("\n\n");
+// ordenaçao por bolha
+  for(k=0;k<TAM-1;k++){
+    for(j=0;j<TAM-1;j++){
+      if(*(pv+j)>*(pv+j+1)){
+        t = *(pv+j);
+        *(pv+j) = *(pv+j+1);
+        *(pv+j+1) = t;
+
+      }
+    }
+  }
+  printf("sequencia crescente => ");
+  for(k=0;k<TAM;k++){
+    printf("%d,",*(pv+k));
+  }
+
+
+  return 0;
+}
